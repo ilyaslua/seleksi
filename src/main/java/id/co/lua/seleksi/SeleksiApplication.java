c7b07e705f3da8683273606306bed4ca564f48f4
birruby <milyas@binaracademy.org>
2024-06-26 14:08:52 +0700

    init app

---
+++ src/main/java/id/co/lua/seleksi/SeleksiApplication.java
@@ -0,0 +1,13 @@
+package id.co.lua.seleksi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeleksiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeleksiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/co/lua/seleksi/SeleksiApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.lua.seleksi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeleksiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
